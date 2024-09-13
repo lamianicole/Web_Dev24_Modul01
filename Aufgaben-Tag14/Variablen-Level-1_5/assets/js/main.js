436309f234c6b24930a8bc784e9237bfa4e744fc
Lamia Rizk <lamiarizk@gmx.de>
2024-09-13 16:02:15 +0200

    bearbeitet

---
+++ Aufgaben-Tag14/Variablen-Level-1_5/assets/js/main.js
@@ -0,0 +1,26 @@
+let carName = "BMW";
+console.log(carName);
+
+let x = 150;
+console.log(x);
+
+let y = 50;
+console.log(y);
+
+let z = x+y;
+console.log(z);
+
+let firstName = "John";
+console.log(firstName);
+
+let lastName = "Doe";
+console.log(lastName);
+
+let age = 35;
+console.log(age);
+
+
+
+
+
+
