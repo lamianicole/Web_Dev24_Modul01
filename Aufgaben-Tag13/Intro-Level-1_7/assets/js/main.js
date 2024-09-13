80a211fc0cc259ff9e3c4bc4db0c300d7197d997
Lamia Rizk <lamiarizk@gmx.de>
2024-09-13 14:31:53 +0200

    bearbeitet

---
+++ Aufgaben-Tag13/Intro-Level-1_7/assets/js/main.js
@@ -0,0 +1,24 @@
+let helloworld = "Hello World";
+console.log(helloworld);
+
+let helloWorld = "Hello Taiwan";
+console.log(helloWorld);
+
+const pi = 3.14;
+console.log(pi);
+
+const burjDubai = "828m";
+console.log(burjDubai);
+// Könnte theoretisch auch let, wenn weiter gebaut wird
+
+const fullName = "Jan Schmidt";
+console.log(fullName);
+
+const eiffelTower = "324m";
+console.log(eiffelTower);
+
+const camelCase = "camelCase";
+console.log(camelCase);
+
+let kursStatus = true;
+console.log(kursStatus);
