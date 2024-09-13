464297eca6dad6bb39d379cb213f94f39f8e6a19
Lamia Rizk <lamiarizk@gmx.de>
2024-09-13 16:03:06 +0200

    bearbeitet

---
+++ Aufgaben-Tag14/Variablen-Level-1_6/assets/js/main.js
@@ -0,0 +1,16 @@
+// let x = 34;
+// console.log(x);
+
+// x = 67;
+// console.log(x);
+// hier reicht x, nicht extra let x = 67
+
+const y = 34;
+console.log(y);
+
+y = 67;
+console.log(y);
+// Hier kommt eine Fehlermeldung, weil eine Konstante nur einmal deklariert werden kann und auch nicht überschrieben werden kann.
+
+
+
