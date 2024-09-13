a1414c1430ce1634500376d45e7025ba74aa2ef7
Lamia Rizk <lamiarizk@gmx.de>
2024-09-13 16:43:37 +0200

    bearbeitet

---
+++ Aufgaben-Tag14/Output-Level-1_4/assets/main.js
@@ -0,0 +1,26 @@
+// window.alert("Hallo Welt");
+
+// window.prompt("Bitte geben Sie Ihren Namen ein.");    
+/* Pop-up-Fenster mit Eingabemöglichkeit */
+
+// window.prompt("Bitte geben Sie Ihren Namen ein", "Cancel");
+
+// window.confirm("Stimmen Sie meiner Meinung zu?");       
+/* Pop-up-Fenster mit Bestätigung/Abbruch */
+
+// let alter = window.prompt( "Gib mir bitte dein Alter:" );  
+// console.log(alter);       
+/* Die ins Pop-up-Fenster eingebene Zahl wird in die Konsole geloggt */
+
+let b = 5;  
+let a = b; 
+console.log(a);
+
+5 -3;  
+window.alert(a);   
+/* Pop-up-Fenster zeigt 5 bzw. Wert von a */
+
+let mann = "Robert Wadlow ist der größte Mann der Welt : ";  
+let grosse = "2,72m";  
+window.confirm(mann + grosse); 
+/* Text von beiden Variablen erscheint im Pop-up-Fenster */
