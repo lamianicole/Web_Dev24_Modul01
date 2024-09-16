2f55d75bf3fc7c88a53236f13b0d97f728649f99
Lamia Rizk <lamiarizk@gmx.de>
2024-09-16 17:13:56 +0200

    bearbeitet

---
+++ Aufgaben_Tag15/DOM-Level-1_2/assets/js/main.js
@@ -0,0 +1,8 @@
+const infoElement = document.getElementById("info")
+infoElement.innerHTML = "<h1>Hello World</h1> <h2>How are you?</h2>"
+// habe den div-info-container mit h1 und h2 befüllt
+
+
+document.getElementById("container").innerHTML = "<p>start of element</p>"
+
+
