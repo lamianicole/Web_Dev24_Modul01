59f6b6a63048b9e669c1e309b85654c5a2cc8205
Lamia Rizk <lamiarizk@gmx.de>
2024-09-16 14:44:10 +0200

    bearbeitet

---
+++ Aufgaben_Tag15/Functions-Level-1_3/assets/js/main.js
@@ -0,0 +1,5 @@
+function intro3(name, stadt, alter){
+    console.log("Hallo, mein Name ist " + name + ". Ich komme aus " + stadt + ". Ich bin " + alter + " Jahre alt.")
+}
+
+intro3("Lamia", "Köln", "40")
