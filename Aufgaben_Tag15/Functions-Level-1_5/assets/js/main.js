b666adfcb1393ba75fbddaaf68ed5552a7d7af5b
Lamia Rizk <lamiarizk@gmx.de>
2024-09-16 15:26:39 +0200

    bearbeitet

---
+++ Aufgaben_Tag15/Functions-Level-1_5/assets/js/main.js
@@ -0,0 +1,60 @@
+// function math(param1, param2){
+//     console.log(param1 * param2) 
+// }
+
+// math(10, 2);
+
+
+
+
+// function math(param1, param2){
+//         console.log(param1 / param2) 
+//     }
+
+// math(10, 2)
+
+
+
+
+// function math(param1, param2){
+//     console.log(param1 * param2) 
+// }
+
+// math(30, 20)
+
+
+
+
+// function math(param1, param2){
+//     console.log(param1 + param2) 
+// }
+
+// math(100, 100)
+
+
+
+
+// function math(param1, param2){
+//     console.log(param1 - param2) 
+//     }
+
+// math(5, 0)
+
+
+
+
+// function math(param1, param2){
+//     console.log(param1 * param2) 
+//     }
+
+// math(45, 173)
+
+
+
+// function math(param1, param2){
+//     console.log(param1 / param2) 
+//     }
+
+// math(1, 1000)
+
+// Ergebnis kommt auch mit allen Nachkommastellen raus 0.001
